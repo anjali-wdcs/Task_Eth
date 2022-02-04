c3a6ce6841eee681bf4882a501428a4fb9180f16
Anjali Sharma <92917156+anjali-wdcs@users.noreply.github.com>
2022-02-04 19:19:34 +0545

    Time Based Token Burning

--- burn.sol
+++ burn.sol
@@ -32,7 +32,7 @@ function div(uint256 a, uint256 b) internal pure returns (uint256) {
  }
 
  function add(uint256 a, uint256 b) internal pure returns (uint256) {
-   uint256 c = a + b;
+   uint256 c = (a + b);
    assert(c >= a);
    return c;
  }
